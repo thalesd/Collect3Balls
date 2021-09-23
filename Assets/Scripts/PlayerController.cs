25163684e5809c53dc1fd4b3078075a402981865
Thales Donizetti <thales.doni.ol@gmail.com>
2021-09-23 11:24:59 -0300

    Creates player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Range(1.5f, 10f)]
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 input = new Vector2();
+
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        if (input.magnitude > 0.1)
+        {
+            transform.Translate(input * speed * Time.deltaTime);
+        }
     }
 }
